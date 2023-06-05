b645d62f3f7111a70630a6fc08302cdd2917a7ac
Paradise <junmate12@gmail.com>
2023-06-05 14:40:04 +0900

    feat: Testing BoardService.findAll()

--- backend/src/apis/board/__test__/board.service.spec.ts
+++ backend/src/apis/board/__test__/board.service.spec.ts
@@ -2,15 +2,27 @@ import { Body, Header } from "@nestjs/common";
 import { BoardController } from "../board.controller";
 import { BoardService } from "../board.service";
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Board } from "../entities/board.entity";
+import { User } from "../../user/entities/user.entity";
 
 class MockBoardRepository {
   mydb = [
+    {
+      number: 5,
+      title: "제목5",
+      content: "내용5",
+      createdAt: "2023-01-05",
+      user: { userId: "철수" },
+      views: 5,
+      image: "http://5",
+    },
     {
       number: 1,
       title: "제목1",
       content: "내용1",
       createdAt: "2023-01-01",
-      userId: "철수",
+      user: { userId: "영희" },
       views: 1,
       image: "http://1",
     },
@@ -19,41 +31,78 @@ class MockBoardRepository {
       title: "제목2",
       content: "내용2",
       createdAt: "2023-01-02",
-      userId: "영희",
+      user: { userId: "영희" },
       views: 2,
       image: "http://2",
     },
   ];
-  findOne({ where }) {}
-}
-
-class MockBoardService {
-  hello() {
-    return "Hello";
+  find({ order: { createdAt: x } }) {
+    if (x === "ASC") {
+      return this.mydb.sort(
+        (b, a) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      );
+    }
   }
 }
 
-describe("BoardController", () => {
-  let boardController: BoardController;
+class MockUserRepository {}
+
+describe("BoardService", () => {
+  let boardService: BoardService;
 
   beforeEach(async () => {
     const boardModule: TestingModule = await Test.createTestingModule({
       controllers: [BoardController],
       providers: [
+        BoardService,
         {
-          provide: BoardService,
-          useClass: MockBoardService,
+          provide: getRepositoryToken(Board),
+          useClass: MockBoardRepository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useClass: MockUserRepository,
         },
       ],
     }).compile();
 
-    boardController = boardModule.get<BoardController>(BoardController);
+    boardService = boardModule.get<BoardService>(BoardService);
   });
 
-  describe("getHello", () => {
-    it("이 테스트의 결과는 success를 반환", () => {
-      const result = boardController.hello(Body, Header);
-      expect(result).toBe("Hello");
+  describe("findAll", () => {
+    it("정렬해서 출력이 잘 되는지 확인", async () => {
+      const result = await boardService.findAll();
+      const expected = [
+        {
+          number: 1,
+          title: "제목1",
+          content: "내용1",
+          createdAt: "2023-01-01",
+          user: { userId: "영희" },
+          views: 1,
+          image: "http://1",
+        },
+        {
+          number: 2,
+          title: "제목2",
+          content: "내용2",
+          createdAt: "2023-01-02",
+          user: { userId: "영희" },
+          views: 2,
+          image: "http://2",
+        },
+        {
+          number: 5,
+          title: "제목5",
+          content: "내용5",
+          createdAt: "2023-01-05",
+          user: { userId: "철수" },
+          views: 5,
+          image: "http://5",
+        },
+      ];
+      expect(result).toStrictEqual(expected);
     });
   });
 });
